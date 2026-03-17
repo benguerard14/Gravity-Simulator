5f6c8ff26a8d346e36c549a3589a02356a48000b
REDF2431 <antoineruel07@gmail.com>
2026-03-17 10:55:51 -0400

    Button

--- GravitySimulator/src/main/java/org/example/gravitysimulator/HelloApplication.java
+++ GravitySimulator/src/main/java/org/example/gravitysimulator/HelloApplication.java
@@ -2,22 +2,44 @@ package org.example.gravitysimulator;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1080, 1920);
-        stage.setTitle("Coucou!");
+    public void start(Stage stage) throws IOException
+    {
+        BorderPane pane = new BorderPane();
+
+        pane.setStyle("-fx-background-color: black;");
+
+
+        Button sandBox = new Button("Sandbox");
+        sandBox.setAlignment(Pos.CENTER);
+
+        sandBox.setStyle("-fx-border-width: 10px;-fx-border-color: white; -fx-text-fill: white; -fx-background-color: transparent;");
+        sandBox.setStyle("-fx-background-radius: 10");
+        sandBox.setPrefSize(200,80);
+        pane.setCenter(sandBox);
+
+
+        Scene scene = new Scene(pane,600,400);
+        stage.setTitle("Gravity Simulator");
         stage.setScene(scene);
-        Button buttonTest = new Button("Button");
+        stage.setFullScreen(true);
         stage.show();
+
+
     }
+
+
+
 
     public static void main(String[] args) {
         launch();
