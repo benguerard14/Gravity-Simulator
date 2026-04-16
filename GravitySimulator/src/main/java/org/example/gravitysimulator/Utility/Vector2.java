8bc305d700823bd9c73ebcb0ea70dcddc0067745
YPezz <yoancoding@gmail.com>
2026-04-16 14:04:55 -0400

    Full fix for Vector2, to be tested

--- GravitySimulator/src/main/java/org/example/gravitysimulator/Utility/Vector2.java
+++ GravitySimulator/src/main/java/org/example/gravitysimulator/Utility/Vector2.java
@@ -6,16 +6,18 @@ public class Vector2 {
     private double y;
 
     public Vector2() {
-        this(0,0);
+        this(0, 0);
     }
 
     public Vector2(double x, double y) {
         this.x = x;
         this.y = y;
     }
+
     public double getX() {
         return x;
     }
+
     public double getY() {
         return y;
     }
@@ -28,50 +30,49 @@ public class Vector2 {
         this.y = y;
     }
 
-    public Vector2 addVector(Vector2 vector) {
-        double newX =  this.x + vector.getX();
-        double newY = this.y + vector.getY();
-        return new Vector2(newX,newY);
-    }
-    public Vector2 subtractVector(Vector2 vector) {
-        double newX =  this.x - vector.getX();
-        double newY = this.y - vector.getY();
-        return new Vector2(newX,newY);
+
+    public Vector2 addVector(Vector2 v) {
+        return new Vector2(this.x + v.x, this.y + v.y);
     }
 
-    public void constMul(double c){
-        this.x *= c;
-        this.y *= c;
+    public Vector2 subtractVector(Vector2 v) {
+        return new Vector2(this.x - v.x, this.y - v.y);
     }
 
-    public static Vector2 directConstMul(Vector2 vector, double c){
-        return new Vector2(vector.x * c, vector.y * c);
+    public Vector2 constMul(double c) {
+        return new Vector2(this.x * c, this.y * c);
+    }
+
+    //Incase
+    public static Vector2 directConstMul(Vector2 v, double c) {
+        return new Vector2(v.x * c, v.y * c);
     }
 
     public double getNorm() {
-        return Math.sqrt((x*x)+(y*y));
+        return Math.sqrt(x * x + y * y);
     }
 
-    public static Vector2 addVector(Vector2 vector1, Vector2 vector2) {
-        return vector1.addVector(vector2);
+    public static Vector2 addVector(Vector2 v1, Vector2 v2) {
+        return new Vector2(v1.x + v2.x, v1.y + v2.y);
     }
 
-
-    public static Vector2 subtractVector(Vector2 vector1, Vector2 vector2) {
-        return vector1.subtractVector(vector2);
+    public static Vector2 subtractVector(Vector2 v1, Vector2 v2) {
+        return new Vector2(v1.x - v2.x, v1.y - v2.y);
     }
 
-    public static double getNorm(Vector2 vector){
-        return vector.getNorm();
+    public static double getNorm(Vector2 v) {
+        return v.getNorm();
     }
 
-    public static Vector2 constMul(Vector2 vector, double c){
-        vector.constMul(c);
-        return vector;
+    // Safe scalar multiply (immutable)
+    public static Vector2 constMul(Vector2 v, double c) {
+        return new Vector2(v.x * c, v.y * c);
     }
 
-    public static Vector2 normalize(Vector2 vector){
-        double n = vector.getNorm();
-        return Vector2.constMul(vector, 1/n);
+    // Safe normalize (immutable)
+    public static Vector2 normalize(Vector2 v) {
+        double n = v.getNorm();
+        if (n == 0) return new Vector2(0, 0);
+        return new Vector2(v.x / n, v.y / n);
     }
 }
